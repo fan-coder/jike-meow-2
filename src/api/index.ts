465ff45a467ce3f8e51ae99feb9a9fff26d042a6
coder-ysj <jailjt.editor@gmail.com>
2019-03-09 17:04:23 +0800

    Add new APIs
    - create session
    - wait for login
    - wait for confirmation
    - refresh token
    - validate token ⇒ api/index.ts ⇒ profile()
    - get following list
    - get follower list
    - unfollow
    - follow

---
+++ src/api/index.ts
@@ -0,0 +1,65 @@
+import axios from "axios";
+
+export default {
+  createSessions: () => axios.get("/sessions.create"),
+  waitForLogin: (uuid: string) =>
+    axios.get(`/sessions.wait_for_login?uuid=${uuid}`),
+  waitForConfirmation: (uuid: string) =>
+    axios.get(`/sessions.wait_for_confirmation?uuid=${uuid}`),
+  refreshToken: () =>
+    axios.get("/app_auth_tokens.refresh", {
+      headers: {
+        "x-jike-refresh-token": localStorage["refreshToken"]
+      }
+    }),
+  profile: () =>
+    axios.get("/1.0/users/profile", {
+      headers: {
+        "x-jike-access-token": localStorage["accessToken"]
+      }
+    }),
+  getFollowingList: (loadMoreKey: string, username: any) =>
+    axios.post(
+      "/1.0/userRelation/getFollowingList",
+      {
+        limit: 20,
+        loadMoreKey: loadMoreKey,
+        username: username
+      },
+      {
+        headers: { "x-jike-access-token": localStorage["accessToken"] }
+      }
+    ),
+  getFollowerList: (loadMoreKey: string, username: any) =>
+    axios.post(
+      "/1.0/userRelation/getFollowerList",
+      {
+        limit: 20,
+        loadMoreKey: loadMoreKey,
+        username: username
+      },
+      {
+        headers: { "x-jike-access-token": localStorage["accessToken"] }
+      }
+    ),
+  unfollow: (username: string) =>
+    axios.post(
+      "/1.0/userRelation/unfollow",
+      {
+        username: username
+      },
+      {
+        headers: { "x-jike-access-token": localStorage["accessToken"] }
+      }
+    ),
+  follow: (username: string) =>
+    axios.post(
+      "/1.0/userRelation/follow",
+      {
+        username: username
+      },
+      {
+        headers: { "x-jike-access-token": localStorage["accessToken"] }
+      }
+    )
+};

5e7af5d78c10765101a90fd132f75b27ebc9e7fc
coder-ysj <jailjt.editor@gmail.com>
2019-03-23 19:40:58 +0800

    add global components

---
+++ src/components/_globals.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import Content from "@/components/notification_referenceItem/Content.vue";
+import Image from "@/components/notification_referenceItem/Image.vue";
+
+Vue.component("meow-nr-content", Content);
+Vue.component("meow-nr-image", Image);

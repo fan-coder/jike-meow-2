f48882127a6a71a9ac45b4071448fdea778085c2
coder-ysj <jailjt.editor@gmail.com>
2019-04-03 17:33:04 +0800

    new actionItem components, new content style

---
+++ src/components/notification_actionItem/ThreeCenter.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="notification-center" :class="{ dark: $store.state.isDarkMode }">
+    <div class="text">
+      <a
+        :href="`https://web.okjike.com/user/${data.actionItem.users[0].username}/post`"
+        target="_blank"
+      >{{ data.actionItem.users[0].screenName }}</a>&nbsp;
+      <span>{{ actionDescription }}</span>
+      <div class="content hidden" v-if="data.actionItem.status === 'HIDDEN'">
+        <span>{{ data.actionItem.content }}</span>
+      </div>
+      <div class="content" v-else>
+        {{ data.actionItem.content || '' }}
+        <span
+          class="content-picture"
+          v-if="data.actionItem.pictures && data.actionItem.pictures.length > 0"
+          @click.self.stop="$store.commit('enlargeImage', data)"
+        >查看图片</span>
+      </div>
+    </div>
+    <div class="time">{{ data.createdAt | reformatTime }}</div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: ["data", "actionDescription"]
+})
+export default class Home extends Vue {}
+</script>
+<style scoped>
+div.notification-center {
+  display: inline-block;
+  vertical-align: top;
+  width: calc(100% - 135px);
+  margin-right: 15px;
+}
+
+div.text {
+  display: block;
+  min-height: 44px;
+  width: 100%;
+  font-size: 14px;
+  line-height: 1.6;
+  color: #808080;
+}
+div.notification-center.dark div.text {
+  color: #bbb;
+}
+div.text > a {
+  color: #000;
+}
+div.notification-center.dark div.text > a {
+  color: #fff;
+}
+div.content {
+  display: block;
+  width: 100%;
+  margin-top: 3px;
+  font-size: 13px;
+  line-height: 1.6;
+  color: #999;
+}
+div.content.hidden > span {
+  padding: 3px 6px;
+  font-size: 12px;
+  line-height: 1.6;
+  color: #fff;
+  background-color: #999;
+  border-radius: 4px;
+}
+span.content-picture {
+  cursor: pointer;
+  color: #409eff;
+}
+
+div.time {
+  display: block;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  font-size: 12px;
+  line-height: 1.6;
+  color: #999;
+}
+div.notification-center.dark div.time {
+  color: #bbb;
+}
+</style>
---
+++ src/components/notification_actionItem/ThreeLeft.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="notification-left" :class="{ dark: $store.state.isDarkMode }">
+    <div class="avatars">
+      <i
+        :style="{backgroundImage: 'url(' + data.actionItem.users[0].profileImageUrl + ')'}"
+        :class="{ isVerified: data.actionItem.users[0].isVerified }"
+      ></i>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: ["data"]
+})
+export default class Home extends Vue {}
+</script>
+<style scoped>
+div.notification-left {
+  display: inline-block;
+  vertical-align: top;
+  width: 35px;
+  margin-right: 15px;
+}
+
+/* 左侧头像 */
+div.avatars {
+  display: block;
+}
+div.avatars > i {
+  cursor: pointer;
+  display: inline-block;
+  vertical-align: middle;
+  height: 35px;
+  width: 35px;
+  border: 0;
+  border-radius: 50%;
+  background: #e1e2e3 center no-repeat;
+  background-size: cover;
+}
+div.avatars > i.isVerified::after {
+  content: "";
+  position: absolute;
+  right: -2px;
+  bottom: -2px;
+  height: 14px;
+  width: 14px;
+  background: url("../../assets/verified.svg") center no-repeat;
+  background-size: 100%;
+  z-index: 5;
+}
+div.notification-left.dark div.avatars > i {
+  background-color: #262626;
+}
+div.avatars > i + i {
+  margin-left: -5px;
+}
+</style>

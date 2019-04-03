4c351db2e13ed6690500ba1db7335e72f3e2b93d
coder-ysj <jailjt.editor@gmail.com>
2019-04-03 18:09:21 +0800

    fix openLink function (dev env)

--- src/function/chrome.ts
+++ src/function/chrome.ts
@@ -2,6 +2,10 @@
 
 export default {
   openLink: (url: string) => {
+    if (process.env.NODE_ENV === "development") {
+      window.open(url);
+      return;
+    }
     chrome.tabs.create({ url: url, active: false });
   }
 };

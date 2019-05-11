ebc01ca7c03de2726d7610f4d0b6aad4de1421df
coder-ysj <jailjt.editor@gmail.com>
2019-05-11 09:38:41 +0800

    update date converter filter

--- src/function/filter.ts
+++ src/function/filter.ts
@@ -1,26 +1,25 @@
 import Vue from "vue";
 
-const reformatTime = (updateTime: string) => {
-  const oldTimestamp = new Date(updateTime).getTime(),
-    newTimestamp = new Date().getTime(),
-    lastTime = newTimestamp - oldTimestamp;
-  if (lastTime < 0) {
+/* Convert timestamp to countdown time or a specific date */
+Vue.filter("reformatTime", (updateTime: string) => {
+  const MESSAGE_DATE = new Date(updateTime).getTime(),
+    NOW_DATE = new Date().getTime(),
+    LAST_TIME = NOW_DATE - MESSAGE_DATE;
+
+  if (LAST_TIME < 0) {
     return "未知时间";
   } else {
-    if (lastTime < 60000) {
+    if (LAST_TIME < 60000) {
       return "刚刚";
-    } else if (lastTime >= 60000 && lastTime < 3600000) {
-      return Math.round(lastTime / 60000) + "分钟前";
-    } else if (lastTime >= 3600000 && lastTime < 86400000) {
-      return Math.round(lastTime / 3600000) + "小时前";
-    } else if (lastTime >= 86400000 && lastTime < 2592000000) {
-      return Math.round(lastTime / 86400000) + "天前";
-    } else if (lastTime >= 2592000000 && lastTime < 31104000000) {
-      return Math.round(lastTime / 2592000000) + "月前";
-    } else if (lastTime >= 31104000000) {
-      return Math.round(lastTime / 31104000000) + "年前";
+    } else if (LAST_TIME >= 60000 && LAST_TIME < 3600000) {
+      return Math.round(LAST_TIME / 60000) + "分钟前";
+    } else if (LAST_TIME >= 3600000 && LAST_TIME < 86400000) {
+      return Math.round(LAST_TIME / 3600000) + "小时前";
+    } else if (LAST_TIME >= 86400000 && LAST_TIME < 2592000000) {
+      return Math.round(LAST_TIME / 86400000) + "天前";
+    } else if (LAST_TIME >= 2592000000) {
+      const FULL_DATE = new Date(MESSAGE_DATE);
+      return `${FULL_DATE.getFullYear()}/${FULL_DATE.getMonth()}/${FULL_DATE.getDay()}`;
     }
   }
-};
-
-Vue.filter("reformatTime", reformatTime);
+});

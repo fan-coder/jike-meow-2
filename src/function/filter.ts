541d32d1ebd1e009efad2b38f0433f4475c7c3ca
coder-ysj <jailjt.editor@gmail.com>
2019-05-11 20:14:22 +0800

    update time converter

--- src/function/filter.ts
+++ src/function/filter.ts
@@ -10,16 +10,22 @@ Vue.filter("reformatTime", (updateTime: string) => {
   if (LAST_TIME < 0) {
     return "未知时间";
   } else {
-    if (LAST_TIME < 60000) {
+    if (LAST_TIME < 120000) {
       return "刚刚";
-    } else if (LAST_TIME >= 60000 && LAST_TIME < 3600000) {
+    } else if (LAST_TIME >= 120000 && LAST_TIME < 3600000) {
       return Math.round(LAST_TIME / 60000) + "分钟前";
     } else if (LAST_TIME >= 3600000 && LAST_TIME < 86400000) {
       return Math.round(LAST_TIME / 3600000) + "小时前";
-    } else if (LAST_TIME >= 86400000 && LAST_TIME < 2592000000) {
+    } else if (LAST_TIME >= 86400000 && LAST_TIME < 259200000) {
       return Math.round(LAST_TIME / 86400000) + "天前";
-    } else if (LAST_TIME >= 2592000000) {
-      return moment(MESSAGE_DATE).format("YYYY.MM.DD");
+    } else if (LAST_TIME >= 259200000) {
+      if (
+        moment(new Date()).format("YYYY") ===
+        moment(MESSAGE_DATE).format("YYYY")
+      ) {
+        return moment(MESSAGE_DATE).format("MM/DD");
+      }
+      return moment(MESSAGE_DATE).format("YYYY/MM/DD");
     }
   }
 });

0f93a9d6708d170c4e968ad1499a484e2aadd924
coder-ysj <jailjt.editor@gmail.com>
2019-05-15 23:22:02 +0800

    add me components

---
+++ src/components/me/bio.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bio" v-if="data.user.bio" :class="{ dark: $store.state.isDarkMode }">
+    <span>{{ data.user.bio }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: ["data"]
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+div.bio {
+  display: block;
+  width: 100%;
+  padding: 10px 30px;
+
+  & > span {
+    font-size: 14px;
+    line-height: 1.6;
+    color: #606266;
+    white-space: pre-wrap;
+  }
+}
+
+/* Dark Theme */
+div.bio.dark {
+  & > span {
+    color: #fff;
+  }
+}
+</style>
---
+++ src/components/me/follow.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="follow" :class="{ dark: $store.state.isDarkMode }">
+    <div @click.stop="$router.push('/me/following?username=' + data.user.username)">
+      <p>关注</p>
+      <span>{{ data.user.statsCount.followingCount > 99999 ? '9999+' : data.user.statsCount.followingCount }}</span>
+    </div>
+    <div @click.stop="$router.push('/me/follower?username=' + data.user.username)">
+      <p>粉丝</p>
+      <span>{{ data.user.statsCount.followedCount > 99999 ? '99999+' : data.user.statsCount.followedCount }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: ["data"]
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+div.follow {
+  display: block;
+  width: 100%;
+  margin-top: 10px;
+  padding: 0 30px;
+  font-size: 0;
+
+  & > div {
+    cursor: pointer;
+    display: inline-block;
+    vertical-align: middle;
+    width: calc(50% - 7.5px);
+    padding: 10px;
+    text-align: center;
+    background-color: #e4e7ed;
+    border-radius: 4px;
+    transition: background-color 0.15s ease-in-out;
+
+    &:hover {
+      color: #303133;
+      background-color: #ffe411;
+    }
+    & > p {
+      display: block;
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 1.6;
+    }
+    & > span {
+      display: block;
+      font-size: 18px;
+      font-weight: 500;
+      line-height: 1.6;
+    }
+  }
+  & > div + div {
+    margin-left: 15px;
+  }
+}
+
+/* Dark Theme */
+div.follow.dark {
+  & > div {
+    background-color: #262626;
+
+    &:hover {
+      background-color: #ffe411;
+    }
+  }
+}
+</style>
